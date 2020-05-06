5869da66f4006be66d362a52021452fecd02d44c
Hesam Alavi <hesamalavi@Hesams-MacBook-Pro.local>
2020-05-06 10:28:20 +1000

    Setup

---
+++ test.js
@@ -0,0 +1,119 @@
+{
+    "type": "root",
+    "children": [{
+            "type": "heading",
+            "depth": 1,
+            "children": [{
+                "type": "text",
+                "value": "Hello World!",
+                "position": {
+                    "start": {
+                        "line": 1,
+                        "column": 3,
+                        "offset": 2
+                    },
+                    "end": {
+                        "line": 1,
+                        "column": 15,
+                        "offset": 14
+                    },
+                    "indent": []
+                }
+            }],
+            "position": {
+                "start": {
+                    "line": 1,
+                    "column": 1,
+                    "offset": 0
+                },
+                "end": {
+                    "line": 1,
+                    "column": 15,
+                    "offset": 14
+                },
+                "indent": []
+            }
+        },
+        {
+            "type": "paragraph",
+            "children": [{
+                    "type": "text",
+                    "value": "This is a ",
+                    "position": {
+                        "start": {
+                            "line": 3,
+                            "column": 1,
+                            "offset": 16
+                        },
+                        "end": {
+                            "line": 3,
+                            "column": 11,
+                            "offset": 26
+                        },
+                        "indent": []
+                    }
+                },
+                {
+                    "type": "link",
+                    "title": null,
+                    "url": "https://google.com",
+                    "children": [{
+                        "type": "text",
+                        "value": "Real page",
+                        "position": {
+                            "start": {
+                                "line": 3,
+                                "column": 12,
+                                "offset": 27
+                            },
+                            "end": {
+                                "line": 3,
+                                "column": 21,
+                                "offset": 36
+                            },
+                            "indent": []
+                        }
+                    }],
+                    "position": {
+                        "start": {
+                            "line": 3,
+                            "column": 11,
+                            "offset": 26
+                        },
+                        "end": {
+                            "line": 3,
+                            "column": 42,
+                            "offset": 57
+                        },
+                        "indent": []
+                    }
+                }
+            ],
+            "position": {
+                "start": {
+                    "line": 3,
+                    "column": 1,
+                    "offset": 16
+                },
+                "end": {
+                    "line": 3,
+                    "column": 42,
+                    "offset": 57
+                },
+                "indent": []
+            }
+        }
+    ],
+    "position": {
+        "start": {
+            "line": 1,
+            "column": 1,
+            "offset": 0
+        },
+        "end": {
+            "line": 4,
+            "column": 1,
+            "offset": 58
+        }
+    }
+}
